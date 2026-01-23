494111eba46feebefce8de982b7fdfd15185d52e
nikhilpatidar7077 <nikhilpatidar7077@gmail.com>
2026-01-24 03:44:55 +0530

    add cors for api use in frontend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require('dotenv').config();
+const cors = require("cors");
 const PORT = process.env.PORT || 3200;
 const connectDB = require("./config/db");
 const userRouter = require("./routes/user.routes");
@@ -16,6 +17,7 @@ const mongoose = require("mongoose");
 
 connectDB();
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/admin", adminRouter);
